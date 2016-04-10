88aff820177e1505e962ea1e7cd1824ec4bf2a81
nalapoke <09poqnat@gmail.com>
2016-04-10 12:11:29 -0500

    Rename main.ts to boostrapper.ts

--- public/scripts/app.component.ts
+++ public/scripts/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
-import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx';   // Load all features
 
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,7 +11,7 @@ import { UserService } from './userCollection/user.service';
   selector: 'dr-app',
   templateUrl: 'scripts/app.html',
   directives: [NavbarComponent, ROUTER_DIRECTIVES],
-  providers: [HTTP_PROVIDERS, UserService]
+  providers: [UserService]
 })
 @RouteConfig([
   { path: '/', name: 'Home', component: HomeComponent, useAsDefault: true },
