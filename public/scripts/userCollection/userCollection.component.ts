b2481b8160207b6c7ba266cd304abc5736bb7224
nalapoke <09poqnat@gmail.com>
2016-04-09 22:49:06 -0500

    Implement userCollection component

--- public/scripts/userCollection/userCollection.component.ts
+++ public/scripts/userCollection/userCollection.component.ts
@@ -1,13 +1,51 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 import { RouteParams } from 'angular2/router';
+
+import { UserService } from './user.service';
+import { IUser } from './interfaces/user';
+import { IUserCollection } from './interfaces/userCollection';
+import { IRelease } from './interfaces/release';
 
 @Component({
   templateUrl: 'scripts/userCollection/userCollection.html'
 })
-export class UserCollectionComponent {
-  public username: string;
+export class UserCollectionComponent implements OnInit{
+  private _pageNumber: number = 1;
 
-  constructor(routeParams: RouteParams) {
+  username: string;
+  user: IUser;
+  userCollection: IRelease[] = [];
+  userError: string;
+  userCollectionError: string;
+
+  constructor(
+    private _userService: UserService,
+    routeParams: RouteParams) {
     this.username = routeParams.get('username');
+  }
+
+  ngOnInit(): void {
+    this._userService.getUser(this.username)
+      .subscribe(
+        (user: IUser) => {
+          this.user = user;
+          this.getUserCollection();
+        },
+        () => this.userError =
+          'Unable to find a Discogs user matching the username \'' + this.username + '\'.');
+  }
+
+  getUserCollection(): void {
+    this._userService.getUserCollection(this.username, this._pageNumber)
+      .subscribe(
+        (userCollection: IUserCollection) => {
+          this.userCollection = this.userCollection.concat(userCollection.releases);
+          if (this._pageNumber < userCollection.pagination.pages) {
+            this._pageNumber++;
+            this.getUserCollection();
+          }
+        },
+        () => this.userCollectionError =
+          'Unable to fetch a collection for Discogs user \'' + this.username + '\'. Please try again.');
   }
 }
